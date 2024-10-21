d6cd931fb622214b6339caa00664786a249cc93d
JuanPorta14 <juanporta.c@gmail.com>
2024-10-20 22:13:37 -0300

    eva puede responder por querys

--- apps/eva2.0/src/app/eva/eva.controller.ts
+++ apps/eva2.0/src/app/eva/eva.controller.ts
@@ -2,11 +2,22 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from '../app.service';
 import { EvaService } from '../services/eva/eva.service';
 import { text } from 'stream/consumers';
+import { assert } from 'console';
 
 @Controller('eva')
 export class EvaController {
 
     constructor(private readonly appService: AppService, private eva: EvaService){}
+    context = [{role: "system", content: "Eres Eva de la pelicula Wall-E"}]
 
+
+    @Get("prompt")
+    async getReply(@Query("prompt") prompt: string){
+        this.context.push({role: "user", content: prompt})
+        const reply = await this.eva.getReply(this.context, 1, 1000)
+        const onlyReply: string = reply.content
+        this.context.push({role: "assistant", content: onlyReply})
+        return onlyReply;
+    }
 }
 
--- apps/eva2.0/src/app/services/eva/eva.service.ts
+++ apps/eva2.0/src/app/services/eva/eva.service.ts
@@ -1,13 +1,40 @@
 import { Get, Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
 import { CHATGPT_KEY } from '../../config';
-
+import { max } from 'rxjs';
+import { Message } from 'openai/resources/beta/threads/messages';
+interface OpenAIReply {
+   choices: [
+      {
+         message: {
+            role: string,
+            content: string
+         }
+      }
+   ]
+}
+ 
 @Injectable()
 export class EvaService {
     
    openai = new OpenAI({apiKey: CHATGPT_KEY});
 
-   
 
+
+   async getReply(context: Array<any>, temperature: number, max_tokens: number){
+      try{ 
+         const reply = await this.openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            temperature:temperature,
+            max_tokens: max_tokens,
+            messages: context
+         })
+         return reply.choices[0].message;
+      } catch(err){
+         console.log(err);
+         throw err;
+      }
+   }
+   
 
 }
